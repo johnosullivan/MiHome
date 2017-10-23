86c8e5af36221617a1892c2e50133ff028edb88b
catemlitten <clitten@luc.edu>
2017-10-23 11:04:09 -0500

    Removed QR Scanner plugin, app now running on Android

--- MiHome_Hybrid_App/src/pages/setup/setup.ts
+++ MiHome_Hybrid_App/src/pages/setup/setup.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CalendarComponentOptions } from 'ion2-calendar';
-import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @Component({
@@ -15,7 +14,7 @@ export class SetupPage {
   optionsRange: CalendarComponentOptions = {
     pickMode: 'range'
   };
-  constructor(private barcodeScanner: BarcodeScanner,private qrScanner: QRScanner,public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private barcodeScanner: BarcodeScanner,public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {

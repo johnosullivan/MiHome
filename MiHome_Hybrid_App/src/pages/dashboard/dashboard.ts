cc001201e545e6bf056dc685c726f154378de28e
catemlitten <clitten@luc.edu>
2017-11-10 11:51:46 -0600

    Placeholder chart added, buttons padded.

--- MiHome_Hybrid_App/src/pages/dashboard/dashboard.ts
+++ MiHome_Hybrid_App/src/pages/dashboard/dashboard.ts
@@ -23,6 +23,9 @@ import * as _ from 'lodash';
 export class DashboardPage {
   @ViewChild(Nav) nav: Nav;
 
+  @ViewChild('dashover') dashCanvas;
+  dashover: any;
+
   start:Date;
   end:Date;
   rootPage: any = DashboardPage;
@@ -237,7 +240,37 @@ export class DashboardPage {
   ionViewDidLoad() {
     this.storeSensorData();
     console.log("Data refreshed");
+    var self = this;
     //store data when dash loads
+    this.dashover = new Chart(this.dashCanvas.nativeElement, {
+      
+                 type: 'doughnut',
+                 data: {
+                     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                     datasets: [{
+                         label: '# of Votes',
+                         data: [12, 19, 3, 5, 2, 3],
+                         backgroundColor: [
+                             'rgba(255, 99, 132, 0.2)',
+                             'rgba(54, 162, 235, 0.2)',
+                             'rgba(255, 206, 86, 0.2)',
+                             'rgba(75, 192, 192, 0.2)',
+                             'rgba(153, 102, 255, 0.2)',
+                             'rgba(255, 159, 64, 0.2)'
+                         ],
+                         hoverBackgroundColor: [
+                             "#FF6384",
+                             "#36A2EB",
+                             "#FFCE56",
+                             "#FF6384",
+                             "#36A2EB",
+                             "#FFCE56"
+                         ]
+                     }]
+                 }
+      
+             });
+      
   }
 
   test() {

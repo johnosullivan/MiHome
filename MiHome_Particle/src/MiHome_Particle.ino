a8a66ead6c48a77cf0e8dd406905f0683323d5e6
John O'Sullivan <josullivan1@luc.edu>
2018-08-31 19:19:52 -0500

    Baseline ino

--- MiHome_Particle/src/MiHome_Particle.ino
+++ MiHome_Particle/src/MiHome_Particle.ino
@@ -1,15 +1,9 @@
 #include <adafruit-sht31.h>
 #include "math.h"
 
-int led1 = D0;
-int led2 = D7;
-
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
 void setup() {
-
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
 
   if (!sht31.begin(0x44)) {
     Serial.println("Couldn't find SHT31");
@@ -19,21 +13,9 @@ void setup() {
 
 void loop() {
 
-  digitalWrite(led1, HIGH);
-  digitalWrite(led2, HIGH);
-
-  delay(2000);
-
-  digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
-
-  delay(2000);
-
-  int x = random(0,1000);
   float t = sht31.readTemperature();
   float h = sht31.readHumidity();
-  float tF = (t* 9) /5 + 32;
-  Particle.publish("random",String(x));
+  float tF = (t * 9) / 5 + 32;
 
   if (!isnan(t)) {
     Particle.publish("Temp *C = ",String(t));
@@ -47,7 +29,6 @@ void loop() {
   } else {
     Serial.println("Failed to read humidity");
   }
-  Serial.println();
 
-  delay(30000);
+  delay(3000);
 }

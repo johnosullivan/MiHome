fbfbef869153a44171a1e3596ed84c858e16ece2
John O'Sullivan <josullivan1@luc.edu>
2018-09-04 22:49:43 -0500

    Added TSL2561

--- MiHome_Particle/src/MiHome_Particle.ino
+++ MiHome_Particle/src/MiHome_Particle.ino
@@ -1,16 +1,17 @@
 #include <ArduinoJson.h>
 #include <Adafruit_CCS811.h>
 #include <adafruit-sht31.h>
+#include <Adafruit_TSL2561_U.h>
 #include "math.h"
 
 #if defined(PARTICLE)
  SYSTEM_THREAD(ENABLED)
 #endif
 
-
 // Setup the instances of the sensors
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
-Adafruit_CCS811 ccs;
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
+//Adafruit_CCS811 ccs;
 
 // Executes setup of the sensors
 void setup() {
@@ -19,30 +20,39 @@ void setup() {
     Serial.println("Couldn't find SHT31");
   }
 
-  if(!ccs.begin()){
-    Serial.println("Couldn't find CCS811");
+  //if(!ccs.begin()){
+  //  Serial.println("Couldn't find CCS811");
+  //}
+
+  if(!tsl.begin()) {
+    Serial.println("Couldn't find TSL2561");
   }
 
   // Calibrate temperature sensor
-  while(!ccs.available());
-  float temp = ccs.calculateTemperature();
-  ccs.setTempOffset(temp - 25.0);
+  //while(!ccs.available());
+  //float temp = ccs.calculateTemperature();
+  //ccs.setTempOffset(temp - 25.0);
+
+  tsl.enableAutoRange(true);
+  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);
+
 }
 
 // Starts the program loop
 void loop() {
 
-  if(ccs.available()){ }
+  //if(ccs.available()){ }
 
   float t = sht31.readTemperature();
   float h = sht31.readHumidity();
   float tF = (t * 9) / 5 + 32;
-  float temp = ccs.calculateTemperature();
+  //float temp = ccs.calculateTemperature();
+
+  sensors_event_t event;
+  tsl.getEvent(&event);
 
   DynamicJsonBuffer jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
-  root["hubID"] = 'MI54768634';
-
   JsonObject& payload = root.createNestedObject("payload");
   JsonArray& fields = payload.createNestedArray("fields");
   JsonObject& data = payload.createNestedObject("data");
@@ -53,18 +63,19 @@ void loop() {
   fields.add("co2");
   fields.add("tvoc");
   fields.add("ppb");
+  fields.add("lux");
 
   data["temperature_celsius"] = t;
   data["temperature_fahrenheit"] = tF;
   data["humidity"] = h;
-  data["co2"] = ccs.geteCO2();
-  data["tvoc"] = ccs.getTVOC();
-  data["ppb"] = temp;
+  data["co2"] = 0;
+  data["tvoc"] = 0;
+  data["lux"] = event.light;
+  data["test"] = "sdfjghjksdagjkhagfhkjgdfhkj";
 
-
-  char jsonbuf[300];
+  char jsonbuf[500];
   root.printTo(jsonbuf,sizeof(jsonbuf));
   Particle.publish("data", jsonbuf);
 
-  delay(30000);
+  delay(10000);
 }

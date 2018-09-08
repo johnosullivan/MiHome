3f1204150e4ec02b333474323c6237ef9374a65f
John O'Sullivan <josullivan1@luc.edu>
2018-09-07 22:04:55 -0500

    Added User Table

---
+++ server_elixir/lib/server_elixir/auth/auth.ex
@@ -0,0 +1,78 @@
+defmodule ServerElixir.Auth do
+  @moduledoc """
+  The Auth context.
+  """
+
+  import Ecto.Query, warn: false
+  alias ServerElixir.Repo
+
+  alias ServerElixir.Auth.User
+
+
+  def authenticate_user(email, password) do
+    query = from(u in User, where: u.email == ^email)
+    query |> Repo.one() |> verify_password(password)
+  end
+
+  defp verify_password(nil, _) do
+    # Perform a dummy check to make user enumeration more difficult
+    Bcrypt.no_user_verify()
+    {:error, "Wrong email or password"}
+  end
+
+  defp verify_password(user, password) do
+    if Bcrypt.verify_pass(password, user.password_hash) do
+      {:ok, user}
+    else
+      {:error, "Wrong email or password"}
+    end
+  end
+
+  def list_users do
+    Repo.all(User)
+  end
+
+  def get_user!(id), do: Repo.get!(User, id)
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a user.
+  ## Examples
+      iex> update_user(user, %{field: new_value})
+      {:ok, %User{}}
+      iex> update_user(user, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+  """
+  def update_user(%User{} = user, attrs) do
+    user
+    |> User.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a User.
+  ## Examples
+      iex> delete_user(user)
+      {:ok, %User{}}
+      iex> delete_user(user)
+      {:error, %Ecto.Changeset{}}
+  """
+  def delete_user(%User{} = user) do
+    Repo.delete(user)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking user changes.
+  ## Examples
+      iex> change_user(user)
+      %Ecto.Changeset{source: %User{}}
+  """
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+end

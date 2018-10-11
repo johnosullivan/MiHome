6888bf7ebba34bc5e618ea505799e3e35b034fc1
John O'Sullivan <josullivan1@luc.edu>
2018-10-10 22:38:06 -0500

    Restructuring NodeJS

--- server_nodejs/index.js
+++ server_nodejs/index.js
@@ -6,4 +6,19 @@ const express = require('express');
 const http = require("http");
 
 const app = express();
+app.use(bodyParser.json());
 
+const port = process.env.PORT || 8080;
+
+const server = app.listen(port, function() {
+  console.log('MiHome Endpoint Server Listening Port:', port);
+});
+
+function shutdown() {
+  server.close();
+};
+
+module.exports = {
+  server,
+  shutdown
+};

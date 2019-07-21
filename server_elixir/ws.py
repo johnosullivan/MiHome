49fa6fc7cfdc200276be4bdb4d3ad2aaa8e70f0d
John O'Sullivan <jnosullivan@icloud.com>
2019-07-20 23:46:40 -0400

    minor updates

--- server_elixir/ws.py
+++ server_elixir/ws.py
@@ -2,48 +2,58 @@
 
 import websockets
 import asyncio
+import json
 
 class WebSocketClient():
 
     def __init__(self):
+        self.join_network_channel = { 
+            "topic":"network",
+            "event":"phx_join",
+            "payload": { },
+            "ref": 0
+        }
+        self.heartbeat_ping = {
+            "topic":"phoenix",
+            "event":"heartbeat",
+            "payload": { },
+            "ref": 0
+        }
         pass
 
-    async def connect(self):
-        '''
-            Connecting to webSocket server - elixir
-        '''
-        self.connection = await websockets.client.connect('ws://127.0.0.1:4000/socket/websocket')
+    async def connect(self, connection_link):
+        # Connecting to webSocket server - elixir 
+        self.connection = await websockets.client.connect(connection_link)
         if self.connection.open:
-            print('Connection Established.')
-            await client.sendMessage("{ \"topic\": \"network\", \"event\": \"phx_join\", \"payload\": {}, \"ref\": 0 }")
+            print(self.connection)
+            await client.sendMessage(json.dumps(self.join_network_channel))
             return self.connection
 
 
     async def sendMessage(self, message):
-        '''
-            Sending message to webSocket server
-        '''
+        # Sending message to webSocket server 
         await self.connection.send(message)
 
     async def receiveMessage(self, connection):
-        '''
-            Receiving all server messages and handling them
-        '''
+        # Receiving all server messages and handling them 
         while True:
             try:
                 message = await connection.recv()
-                print('Received message from server: ' + str(message))
+                payload = json.loads(str(message))
+
+
+                print(payload)
+
             except websockets.exceptions.ConnectionClosed:
                 break
 
     async def heartbeat(self, connection):
-        '''
-        Sending heartbeat to server every 30 seconds to avoid disconnect
-        '''
+        # Sending heartbeat to server every 30 seconds to avoid disconnect
         while True:
             try:
-                await connection.send("{\"topic\": \"phoenix\",\"event\": \"heartbeat\", \"payload\": {},\"ref\": 0 }")
+                await connection.send(json.dumps(self.heartbeat_ping))
                 await asyncio.sleep(30)
+
             except websockets.exceptions.ConnectionClosed:
                 break
 
@@ -53,7 +63,7 @@ if __name__ == '__main__':
     client = WebSocketClient()
     loop = asyncio.get_event_loop()
 
-    connection = loop.run_until_complete(client.connect())
+    connection = loop.run_until_complete(client.connect('ws://127.0.0.1:4000/socket/websocket'))
 
     # Start listener and heartbeat
     tasks = [

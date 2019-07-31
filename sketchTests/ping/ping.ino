2a3a9043eac759e4a2046e2b9cf8a77d983dd715
John O'Sullivan <jnosullivan@icloud.com>
2019-07-30 21:55:43 -0400

    ping.ino test update

--- sketchTests/ping/ping.ino
+++ sketchTests/ping/ping.ino
@@ -1,6 +1,6 @@
 #include <SPI.h>
 #include <RH_RF69.h>
-
+#include <Adafruit_BMP085.h>
 
 #define RF69_FREQ 915.0
 #define RFM69_INT     3
@@ -11,8 +11,9 @@
 
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
 
+Adafruit_BMP085 bmp;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Starting...");
   pinMode(RFM69_RST, OUTPUT);
@@ -21,7 +22,6 @@ void setup() {
   Serial.println("RFM69 RX Test!");
   Serial.println();
 
-  // manual reset
   digitalWrite(RFM69_RST, HIGH);
   delay(10);
   digitalWrite(RFM69_RST, LOW);
@@ -36,23 +36,30 @@ void setup() {
   if (!rf69.setFrequency(RF69_FREQ)) {
     Serial.println("Set Frequency Initialization Failed!");
   }
+  
+  rf69.setTxPower(20, true); 
 
-  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
-  // ishighpowermodule flag set like this:
-  rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
-
-  // The encryption key has to be the same as the one in the server
   uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
   rf69.setEncryptionKey(key);
 
   Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
+
+  if (!bmp.begin()) 
+  {
+    Serial.println("BMP180 Init Failed");
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  char radiopacket[20] = "Hello World #";
-  Serial.print("Sending "); Serial.println(radiopacket);
-  rf69.send((uint8_t *)radiopacket, strlen(radiopacket));
+  float temp = bmp.readTemperature();
+  
+  String payload = String(temp) + ",0,0";
+  uint8_t data[payload.length()];
+  payload.getBytes(data, payload.length());
+  rf69.send(data, sizeof(data));
   rf69.waitPacketSent();
+
+  Serial.println(payload);
+ 
   delay(5000);
 }

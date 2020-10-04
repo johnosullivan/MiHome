671eaa44f32e5be3b5efa160aebb8c2778ab7c48
John O'Sullivan <jnosullivan@icloud.com>
2020-10-03 21:53:14 -0500

    Update main.go

--- server_go/main.go
+++ server_go/main.go
@@ -107,33 +107,6 @@ func ParseRsaPublicKeyFromPemStr(pubPEM string) (*rsa.PublicKey, error) {
 }
 
 func main() {
-	/*
-	// Create the keys
-    priv, pub := GenerateRsaKeyPair()
-
-    // Export the keys to pem string
-    priv_pem := ExportRsaPrivateKeyAsPemStr(priv)
-    pub_pem, _ := ExportRsaPublicKeyAsPemStr(pub)
-
-    // Import the keys from pem string
-    priv_parsed, _ := ParseRsaPrivateKeyFromPemStr(priv_pem)
-    pub_parsed, _ := ParseRsaPublicKeyFromPemStr(pub_pem)
-
-    // Export the newly imported keys
-    priv_parsed_pem := ExportRsaPrivateKeyAsPemStr(priv_parsed)
-    pub_parsed_pem, _ := ExportRsaPublicKeyAsPemStr(pub_parsed)
-
-    fmt.Println(priv_parsed_pem)
-    fmt.Println(pub_parsed_pem)
-
-    // Check that the exported/imported keys match the original keys
-    if priv_pem != priv_parsed_pem || pub_pem != pub_parsed_pem {
-            fmt.Println("Failure: Export and Import did not result in same Keys")
-    } else {
-            fmt.Println("Success")
-    }
-    */
-
 	errenv := godotenv.Load()
   	if errenv != nil { }
 
